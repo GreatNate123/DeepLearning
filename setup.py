91def346e7ec68d9619f796a99773063d59e180f
GreatNate123 <68345403+GreatNate123@users.noreply.github.com>
2022-07-11 18:31:19 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name="DeepLearning",
+  author="Nathaniel Black",
+  author="nathanblack921@gmail.com",
+  license="MIT License",
+  description="This is my math library.",
+  packages=["DeepLearning]
+)
